e106bb2195cf38095d15c847bf4f7f2c5b810f54
123 <123@qq.com>
2020-10-04 17:11:04 +0800

    sout方法认识

---
+++ java_basic/src/com/ho/DoObject.java
@@ -0,0 +1,26 @@
+package com.ho;
+
+/**
+ * sout方法探索
+ */
+public class DoObject {
+
+    public static void main(String[] args) {
+
+
+
+        char[] ch = new char[]{'1','2','3'};
+
+        // 对于char数组，打印的是数值
+        System.out.println(ch);
+
+        int[] ints = new int[]{1,45,67,88,33};
+
+        // 对于int数组，打印的时地址值
+        System.out.println(ints);
+
+
+
+
+    }
+}
