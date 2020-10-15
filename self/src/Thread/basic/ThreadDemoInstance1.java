7508f7e2066c28c7f466849769fd0816cc8eed29
Mr-Ho-go <70848466+mr-ho-go@users.noreply.github.com>
2020-10-15 11:58:35 +0800

    更改

--- self/src/Thread/basic/ThreadDemoInstance1.java
+++ self/src/Thread/basic/ThreadDemoInstance1.java
@@ -2,8 +2,15 @@ package Thread.basic;
 
 
 public class ThreadDemoInstance1 extends Thread {
+
+
+
     @Override
     public void run(){
+        // 显示线程名称
         System.out.println("-----ThreadDemoInstance1-------"+Thread.currentThread().getName()+"======"+Thread.currentThread().getId());
     }
+
+
+
 }
